ca01f9a82a9da10cc7eb0feace834baa2bb8e1fe
alena-suhih <prhalenka23@gmail.com>
2023-03-03 15:06:23 +0200

    fixed tests

--- gh.test.js
+++ gh.test.js
@@ -1,5 +1,9 @@
 let page;
 
+afterEach(() => {
+  page.close();
+});
+
 describe("Github page tests", () => {
   beforeEach(async () => {
     page = await browser.newPage();
@@ -11,63 +15,51 @@ describe("Github page tests", () => {
   });
 
   test("The h1 header content'", async () => {
-    setTimeout(async () => {
-      const firstLink = await page.$("header div div a");
-      await firstLink.click();
-      await page.waitForSelector("h1");
-      const title2 = await page.title();
-      expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
-    }, 60000);
-  });
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
+    expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
+  }, 60000);
 
   test("The first link attribute", async () => {
-    setTimeout(async () => {
-      const actual = await page.$eval("a", (link) => link.getAttribute("href"));
-      expect(actual).toEqual("#start-of-content");
-    }, 70000);
-  });
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
+    expect(actual).toEqual("#start-of-content");
+  }, 70000);
 
   test("The page contains Sign in button", async () => {
-    setTimeout(async () => {
-      const btnSelector = ".btn-large-mktg.btn-mktg";
-      await page.waitForSelector(btnSelector, {
-        visible: true,
-      });
-      const actual = await page.$eval(btnSelector, (link) => link.textContent);
-      expect(actual).toContain("Get started with Team");
-    }, 80000);
-  });
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  }, 80000);
 });
 
 test("The h1 header content About page", async () => {
-  setTimeout(async () => {
-    page = await browser.newPage();
-    await page.goto("https://github.com/about");
-    await page.$("header div div a");
-    await page.waitForSelector("h1");
-    const title2 = await page.title();
-    expect(title2).toEqual("Let's build from here");
-  }, 60000);
-});
+  page = await browser.newPage();
+  await page.goto("https://github.com/about");
+  await page.$("header div div a");
+  await page.waitForSelector("h1");
+  const title2 = await page.title();
+  expect(title2).toEqual("About · GitHub");
+}, 60000);
 
 test("The h1 header content Skills page", async () => {
-  setTimeout(async () => {
-    page = await browser.newPage();
-    await page.goto("https://skills.github.com/");
-    await page.$("header div div a");
-    await page.waitForSelector("h1");
-    const title2 = await page.title();
-    expect(title2).toEqual("GitHub Skills");
-  }, 60000);
-});
+  page = await browser.newPage();
+  await page.goto("https://skills.github.com/");
+  await page.$("header div div a");
+  await page.waitForSelector("h1");
+  const title2 = await page.title();
+  expect(title2).toEqual("GitHub Skills");
+}, 60000);
 
 test("The h1 header content Services page", async () => {
-  setTimeout(async () => {
-    page = await browser.newPage();
-    await page.goto("https://github.com/services/");
-    await page.$("header div div a");
-    await page.waitForSelector("h1");
-    const title2 = await page.title();
-    expect(title2).toEqual("The GitHub" < br > "Expert Services Team");
-  }, 60000);
-});
+  page = await browser.newPage();
+  await page.goto("https://github.com/services/");
+  await page.$("header div div a");
+  await page.waitForSelector("h1");
+  const title2 = await page.title();
+  expect(title2).toEqual("Expert Services | GitHub · GitHub");
+}, 60000);
